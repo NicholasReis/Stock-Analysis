42756ee02239acd0c30cc0c67db757509206c52d
Nicholas <46433505+NicholasReis@users.noreply.github.com>
2021-05-06 12:57:33 -0700

    This now holds a stock object.
    Wrote out the gameplan and decided to use OOP. I have a new prototype I am pushing out.

--- StockAnalysis/Stock.py
+++ StockAnalysis/Stock.py
@@ -1,73 +1,22 @@
-import requests
-import time
-import json
+from DataGrabber import DataGrabber
 
-def main():
-	print("Started")
-	acquireData("amc")
+class Stock:
+	stock_symbol = ""
+	def __init__(self, stock_symbol):
+		self.stock_symbol = stock_symbol
+		DataGrabber(stock_symbol)
 
+		#I need to break these all down into each year
+		#So with some fandangling I can use year as an
+		#Identifier of sorts and the labels below as the
+		#Key I want returned
 
-	#time.sleep(5)
-	
-	
-	
-	
-	#time.sleep(5)
-	#cash_flow = requests.get('https://finance.yahoo.com/quote/'+ stock_symbol +'/cash-flow?p='+ stock_symbol)
-	#print("Cash Flow found!")
-	
-	#print("All information recieved! Processing...");
-
-def acquireData(stock_symbol):
-	income_statement = requests.get('https://finance.yahoo.com/quote/'+ stock_symbol +'/financials?p='+ stock_symbol)
-	print("Income Statement found!")
-
-	begin = income_statement.text.index("annualTotalRevenue")
-	end = income_statement.text.index("]", begin)
-	
-	output = income_statement.text[begin:end+1]
-	annualTotalRevenue = cleanData(output)
-	
-	#annualInterestExpenseNonOperating
-	
-	#balance_sheet = requests.get('https://finance.yahoo.com/quote/'+ stock_symbol +'/balance-sheet?p='+ stock_symbol)
-	#print("Balance Sheet found!")
-
-def cleanData(output):
-	print("Cleaning")
-	begin = output.index("[")
-	end = output.index("]")
-	output = output[begin +1:end]
-	
-	pieces = []
-	openBraces = 0
-	beginPiece = 0
-	closePiece = 0
-	index = 0
-	while(index < len(output)):
-		if(output[index] =="{"):
-			if(openBraces ==0):
-				beginPiece = index
-			openBraces += 1
-		elif(output[index] =="}"):
-			openBraces -= 1
-			if(openBraces ==0):
-				closePiece = index
-				print(output[beginPiece:closePiece+1])
-				pieces.append(output[beginPiece:closePiece+1])
-		index +=1
-	
-	data =[]
-	for piece in pieces:
-		f = json.loads(str(piece))
-		print(type(f))
-		print(f["reportedValue"]["raw"])
-
-	
-				
-
-
-
-
-if __name__ == "__main__":
-    main()
+		#pe
+		#annual_revenue
+		#profit_margin #(Net income / Total Revenue)
+		#shares_issued
+		#current_assets
+		#current_liabilities
+		#cash_flow
+		#capital_expenditures
+		#price #price to free cash flow < 15%
