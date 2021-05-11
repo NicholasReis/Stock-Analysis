b435f50778f263348adb1fa81448211406221365
NicholasReis <NicholasCReis@gmail.com>
2021-05-10 22:15:51 -0700

    Shits fucked and so am I. I created a temporary file to test data and build the functionality so I could take a break from the nightmare that is reading the JSON formatting the yahoo.finance team produced. I will continue working on reading the info from the file since I could later just output to a file once I format the yahoo.IHateUserInteraction string.
    I am close, but python is a cruel bitch. We'll figure her out though. Eventually.

--- StockAnalysis/Stock.py
+++ StockAnalysis/Stock.py
@@ -1,11 +1,35 @@
 from DataGrabber import DataGrabber
 
 class Stock:
+	yearlyData = dict()
+	
 	stock_symbol = ""
 	def __init__(self, stock_symbol):
 		self.stock_symbol = stock_symbol
-		DataGrabber(stock_symbol)
+		self.loadFromFile()
+		#DataGrabber(stock_symbol)
+	
+	def loadFromFile(self):
+		lines =[]
+		with open('temporaryData') as tempData:
+			lines.append(tempData.read().splitlines())
 
+		year = 0
+		for line in lines:
+			print("Newline")
+			if(line != ""):
+				values = str(line).split(":")
+				print(values)
+				if(values[0] == "year"):
+					year = values[1]
+					self.yearlyData.append(year)
+					print(yearlyData)
+				else:
+					print(values)
+					self.yearlyData[year].append(values[0])
+					self.yearlyData[year][values[0]] = values[1]
+
+		print(self.yearlyData["2020"])
 		#I need to break these all down into each year
 		#So with some fandangling I can use year as an
 		#Identifier of sorts and the labels below as the
--- StockAnalysis/StockAnalyzer.py
+++ StockAnalysis/StockAnalyzer.py
@@ -1,10 +1,12 @@
 import requests
 
 from Stock import Stock
+#from Stock import Analyzer
 
 def main():
 	print("Started")
 	stock = Stock("amc")
+	#processedData = Analyzer()
 
 if __name__ == "__main__":
     main()
